d1abcdf0d9fdfcc8403c08ad19cd371571d51f06
SVS <victorviccc1@gmail.com>
2023-09-17 10:14:08 +0300

    Adds ViewModel factory

---
+++ app/src/main/java/com/severin/movies/presentation/vm/MoviesViewModelFactory.kt
@@ -0,0 +1,9 @@
+package com.severin.movies.presentation.vm
+
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider
+
+class MoviesViewModelFactory constructor(
+    application: Application
+) : ViewModelProvider.Factory {
+}
