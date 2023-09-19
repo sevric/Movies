4634551eb855f39e2396baab2069d2a418b35463
SVS <victorviccc1@gmail.com>
2023-09-20 00:01:54 +0300

    Adds view models for Favourites screen

--- app/src/main/java/com/severin/movies/presentation/ui/FavouritesFragment.kt
+++ app/src/main/java/com/severin/movies/presentation/ui/FavouritesFragment.kt
@@ -5,12 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import com.severin.movies.databinding.FragmentFavouritesBinding
+import com.severin.movies.presentation.MovieApplicationGlobal
+import com.severin.movies.presentation.vm.FavouriteMoviesViewModel
+import com.severin.movies.presentation.vm.MovieByIdFromApiViewModel
+import com.severin.movies.presentation.vm.MoviesViewModelFactory
 
 class FavouritesFragment : Fragment() {
 
     private var _binding: FragmentFavouritesBinding? = null
     private val binding get() = _binding!!
+
+    private val movieApplicationGlobal: MovieApplicationGlobal by lazy {
+        MovieApplicationGlobal.instance
+    }
+    private val favouriteMoviesViewModel: FavouriteMoviesViewModel by activityViewModels {
+        MoviesViewModelFactory(movieApplicationGlobal)
+    }
+    private val movieByIdFromApiViewModel: MovieByIdFromApiViewModel by viewModels {
+        MoviesViewModelFactory(movieApplicationGlobal)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,8 +36,21 @@ class FavouritesFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        favouriteMoviesViewModel.getAllFavouriteMovies()
+        observeViewModelFavouriteMovies()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun observeViewModelFavouriteMovies() {
+        favouriteMoviesViewModel.allFavouriteMovies.observe(viewLifecycleOwner) {
+            //TODO(submitList(it) for favourite movies Adapter)
+        }
     }
 }
