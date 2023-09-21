e758b82ebd47e0a3113e9926e7f03e73138b58b6
SVS <victorviccc1@gmail.com>
2023-09-21 12:00:00 +0300

    Adds adapter for FavouritesFragment

--- app/src/main/java/com/severin/movies/presentation/ui/FavouritesFragment.kt
+++ app/src/main/java/com/severin/movies/presentation/ui/FavouritesFragment.kt
@@ -7,8 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import com.severin.movies.data.model.MovieItemDB
 import com.severin.movies.databinding.FragmentFavouritesBinding
 import com.severin.movies.presentation.MovieApplicationGlobal
+import com.severin.movies.presentation.adapters.MovieFragmentStarter
+import com.severin.movies.presentation.adapters.MovieFromDBAdapterClickListener
+import com.severin.movies.presentation.adapters.MovieFromDbAdapter
 import com.severin.movies.presentation.vm.FavouriteMoviesViewModel
 import com.severin.movies.presentation.vm.MovieByIdFromApiViewModel
 import com.severin.movies.presentation.vm.MoviesViewModelFactory
@@ -28,6 +33,25 @@ class FavouritesFragment : Fragment() {
         MoviesViewModelFactory(movieApplicationGlobal)
     }
 
+    private val movieFromDBAdapterClickListener by lazy {
+        object : MovieFromDBAdapterClickListener {
+            override fun onClick(movieItemDB: MovieItemDB) {
+                MovieFragmentStarter(
+                    this@FavouritesFragment
+                ).startMovieFragment(movieItemDB.movieDbId)
+            }
+
+            override fun onLongClick(movieItemDB: MovieItemDB) {
+                MovieFragmentStarter(
+                    this@FavouritesFragment
+                ).startMovieBottomSheetFragment(movieItemDB.movieDbId)
+            }
+        }
+    }
+    private val favouriteMoviesAdapter by lazy {
+        MovieFromDbAdapter(movieFromDBAdapterClickListener)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,6 +63,7 @@ class FavouritesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        preparePopularMoviesAdapter()
         favouriteMoviesViewModel.getAllFavouriteMovies()
         observeViewModelFavouriteMovies()
     }
@@ -48,9 +73,23 @@ class FavouritesFragment : Fragment() {
         _binding = null
     }
 
+    private fun preparePopularMoviesAdapter() {
+        binding.rvFavourites.apply {
+            layoutManager = GridLayoutManager(
+                requireContext(),
+                SPAN_COUNT_FOR_GRID_ADAPTER
+            )
+            adapter = favouriteMoviesAdapter
+        }
+    }
+
     private fun observeViewModelFavouriteMovies() {
         favouriteMoviesViewModel.allFavouriteMovies.observe(viewLifecycleOwner) {
-            //TODO(submitList(it) for favourite movies Adapter)
+            favouriteMoviesAdapter.submitList(it)
         }
+    }
+
+    companion object {
+        private const val SPAN_COUNT_FOR_GRID_ADAPTER = 2
     }
 }
