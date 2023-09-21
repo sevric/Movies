bfffa98e449b80706b231e4771337ef3d0b6586d
SVS <victorviccc1@gmail.com>
2023-09-21 11:50:20 +0300

    Adds adapter for GenresFragment

--- app/src/main/java/com/severin/movies/presentation/ui/GenresFragment.kt
+++ app/src/main/java/com/severin/movies/presentation/ui/GenresFragment.kt
@@ -6,8 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import com.severin.movies.data.model.Genre
 import com.severin.movies.databinding.FragmentGenresBinding
 import com.severin.movies.presentation.MovieApplicationGlobal
+import com.severin.movies.presentation.adapters.GenresAdapter
+import com.severin.movies.presentation.adapters.GenresAdapterClickListener
+import com.severin.movies.presentation.adapters.MovieFragmentStarter
 import com.severin.movies.presentation.vm.GenresViewModel
 import com.severin.movies.presentation.vm.MoviesViewModelFactory
 
@@ -23,6 +28,19 @@ class GenresFragment : Fragment() {
         MoviesViewModelFactory(movieApplicationGlobal)
     }
 
+    private val genresAdapterClickListener by lazy {
+        object : GenresAdapterClickListener {
+            override fun onClick(genre: Genre) {
+                MovieFragmentStarter(
+                    this@GenresFragment
+                ).startMoviesByGenreFragment(genre.id, genre.name)
+            }
+        }
+    }
+    private val genresAdapter by lazy {
+        GenresAdapter(genresAdapterClickListener)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -34,18 +52,33 @@ class GenresFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        prepareGenresAdapter()
         genresViewModel.getGenres()
         observeGenresViewModel()
-    }
-
-    private fun observeGenresViewModel() {
-        genresViewModel.genresFromApi.observe(viewLifecycleOwner) {
-            //TODO(submitList(it.genres) for genres Adapter)
-        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun prepareGenresAdapter() {
+        binding.rvGenres.apply {
+            layoutManager = GridLayoutManager(
+                requireContext(),
+                SPAN_COUNT_FOR_GRID_ADAPTER
+            )
+            adapter = genresAdapter
+        }
+    }
+
+    private fun observeGenresViewModel() {
+        genresViewModel.genresFromApi.observe(viewLifecycleOwner) {
+            genresAdapter.submitList(it.genres)
+        }
+    }
+
+    companion object {
+        private const val SPAN_COUNT_FOR_GRID_ADAPTER = 2
     }
 }
