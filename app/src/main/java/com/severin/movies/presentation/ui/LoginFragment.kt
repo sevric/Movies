accb45858245cb973a20266428281c036afca455
SVS <victorviccc1@gmail.com>
2023-09-24 00:17:24 +0300

    Adds view model for Login screen

--- app/src/main/java/com/severin/movies/presentation/ui/LoginFragment.kt
+++ app/src/main/java/com/severin/movies/presentation/ui/LoginFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.severin.movies.R
 import com.severin.movies.databinding.FragmentLoginBinding
+import com.severin.movies.presentation.vm.FirebaseAuthorizationViewModel
 
 class LoginFragment : Fragment() {
     private var emailToLogin: String? = null
@@ -16,6 +19,12 @@ class LoginFragment : Fragment() {
 
     private lateinit var emailString: String
     private lateinit var passwordString: String
+
+    private val firebaseAuthorizationViewModel: FirebaseAuthorizationViewModel by lazy {
+        ViewModelProvider(
+            requireActivity(),
+        )[FirebaseAuthorizationViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +38,13 @@ class LoginFragment : Fragment() {
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setClickListeners()
+        observeLoginViewModel()
     }
 
     override fun onDestroyView() {
@@ -57,10 +73,87 @@ class LoginFragment : Fragment() {
         }
     }
 
+    private fun setClickListeners() {
+        binding.btnLogin.setOnClickListener {
+            emailString = getLoginStringFromView()
+            passwordString = binding.inputPassword.text.toString()
+
+            if (emailString.isNotEmpty()
+                || emailString != EMPTY_STRING
+                || passwordString.isNotEmpty()
+                || passwordString != EMPTY_STRING
+            ) {
+                firebaseAuthorizationViewModel.login(emailString, passwordString)
+            } else {
+                Toast.makeText(
+                    requireActivity(),
+                    REQUEST_FILL_FIELDS_NOTIFICATION,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
+        binding.tvForgotPassword.setOnClickListener {
+            emailString = getLoginStringFromView()
+
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(
+                    R.id.main_navigation_view,
+                    ResetPasswordFragment.newInstance(
+                        emailString
+                    )
+                )
+                .addToBackStack(null)
+                .commit()
+        }
+
+        binding.tvRegisterLink.setOnClickListener {
+            emailString = getLoginStringFromView()
+
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(
+                    R.id.main_navigation_view,
+                    RegistrationFragment.newInstance(
+                        emailString
+                    )
+                )
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+
+    private fun getLoginStringFromView(): String {
+        return binding.inputEmailOrLogin.text.toString()
+    }
+
+    private fun observeLoginViewModel() {
+        firebaseAuthorizationViewModel.error.observe(this) {
+            if (it != null) {
+                Toast.makeText(requireActivity(), it, Toast.LENGTH_LONG).show()
+            }
+        }
+
+        firebaseAuthorizationViewModel.currentFirebaseAuthUser.observe(this) {
+            if (it != null) {
+                requireActivity().supportFragmentManager.beginTransaction()
+                    .replace(
+                        R.id.main_navigation_view,
+                        PersonalPageFragment.newInstance()
+                    )
+                    .addToBackStack(PERSONAL_PAGE_FRAGMENT_BACKSTACK_NAME)
+                    .commit()
+            }
+        }
+    }
+
     companion object {
         private const val EMAIL_TO_LOGIN_STR_KEY = "emailToResetStr"
         private const val NOTIFICATION_NO_EMAIL = "No email were entered"
         private const val REQUEST_PROVIDE_EMAIL = "Provide your email"
+        private const val EMPTY_STRING = ""
+        private const val REQUEST_FILL_FIELDS_NOTIFICATION =
+            "The required fields should be filled!"
+        private const val PERSONAL_PAGE_FRAGMENT_BACKSTACK_NAME = "PersonalPageFragment"
 
         @JvmStatic
         fun newInstance(emailToLogin: String) =
