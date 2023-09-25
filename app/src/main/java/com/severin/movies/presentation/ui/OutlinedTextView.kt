a80c7ff7159a2eae0560fe86852e14e7c124d953
SVS <victorviccc1@gmail.com>
2023-09-26 02:46:03 +0300

    Fixes MovieFragment title edges

--- app/src/main/java/com/severin/movies/presentation/ui/OutlinedTextView.kt
+++ app/src/main/java/com/severin/movies/presentation/ui/OutlinedTextView.kt
@@ -20,27 +20,55 @@ class OutlinedTextView @JvmOverloads constructor(
     }
     override fun draw(canvas: Canvas) {
 
+        //Setting coordinates for the text extended by invisible dots
         val x = ZERO_COORDINATE
         val y = textSize
         val currentText = text.toString()
 
+        //Setting actual width of the View as by the text extended by transparent dots
+        val dummyTextExtendedByStartEndDots =
+            String.format(DUMMY_TEXT_EXTENDED_BY_START_END_DOTS_TEMPLATE, currentText)
+        width = outlinedTitlePaint.measureText(dummyTextExtendedByStartEndDots).toInt()
+
+        //Installing general text presets
         outlinedTitlePaint.textAlign = Paint.Align.LEFT
-        outlinedTitlePaint.strokeWidth = STROKE_WIDTH
         outlinedTitlePaint.textSize = textSize
+
+        //Drawing transparent dot at the start edge
+        outlinedTitlePaint.color = Color.TRANSPARENT
+        outlinedTitlePaint.style = Paint.Style.FILL
+        canvas.drawText(TRANSPARENT_DOT_TEXT_TO_DRAW, x, y, outlinedTitlePaint)
+
+        //Drawing actual visible text stroke (outline)
+        val xVisibleText = outlinedTitlePaint.measureText(TRANSPARENT_DOT_TEXT_TO_DRAW)
         outlinedTitlePaint.style = Paint.Style.STROKE
+        outlinedTitlePaint.strokeWidth = STROKE_WIDTH
         outlinedTitlePaint.strokeJoin = Paint.Join.ROUND
         outlinedTitlePaint.color = Color.WHITE
-        canvas.drawText(currentText, x, y, outlinedTitlePaint)
+        canvas.drawText(currentText, xVisibleText, y, outlinedTitlePaint)
 
+        //Drawing actual text fill (the text itself)
         outlinedTitlePaint.style = Paint.Style.FILL
         outlinedTitlePaint.color = Color.BLACK
-        canvas.drawText(currentText, x, y, outlinedTitlePaint)
+        canvas.drawText(currentText, xVisibleText, y, outlinedTitlePaint)
+
+        //Setting new x coordinate for drawing the end dot
+        val dummyTextExtendedByStartDot =
+            String.format(DUMMY_TEXT_EXTENDED_BY_START_DOT_TEMPLATE, currentText)
+        val xTransparentEndDot = outlinedTitlePaint.measureText(dummyTextExtendedByStartDot)
+        //Drawing transparent dot at the end edge
+        outlinedTitlePaint.color = Color.TRANSPARENT
+        outlinedTitlePaint.style = Paint.Style.FILL
+        canvas.drawText(TRANSPARENT_DOT_TEXT_TO_DRAW, xTransparentEndDot, y, outlinedTitlePaint)
 
         super.draw(canvas)
     }
 
     companion object {
         private const val ZERO_COORDINATE: Float = 0f
-        private const val STROKE_WIDTH: Float = 15F
+        private const val STROKE_WIDTH: Float = 10F
+        private const val DUMMY_TEXT_EXTENDED_BY_START_END_DOTS_TEMPLATE = ".%1\$s."
+        private const val DUMMY_TEXT_EXTENDED_BY_START_DOT_TEMPLATE = ".%1\$s"
+        private const val TRANSPARENT_DOT_TEXT_TO_DRAW = "."
     }
 }
